01d8227fd29ab1f25cd876f191746c8be2a0689f
User <den_m1985@mail.ru>
2024-06-11 15:00:48 +0200

    resolve problem

---
+++ src/main/java/easy/relative_sort_array/Example1.java
@@ -0,0 +1,53 @@
+package easy.relative_sort_array;
+
+import java.util.Arrays;
+
+public class Example1 {
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length];
+        int[] countNotMatches = new int[arr1.length];
+
+        //sort  [1 2 2 2 3 3 4 6 7 9 19]
+        Arrays.sort(arr1);
+
+        //find same digit and write to new array  [2 2 2 1 4 3 3 9 6 0 0]
+        int count = 0;
+        for (int digitArr2 : arr2) {
+            for (int j = 0; j < arr1.length; j++) {
+                if (arr1[j] == digitArr2) {
+                    result[count] = digitArr2;
+                    count++;
+                    countNotMatches[j]++;
+                }
+            }
+        }
+
+        // find count digit no matches  [1 1 1 1 1 1 1 1 0 1 0]
+        int countDigit = 0;
+        for (int countNotMatch : countNotMatches) {
+            if (countNotMatch == 0) {
+                countDigit++;
+            }
+        }
+
+        // write digit no use to new array  [7 19]
+        int[] digitsNoUse = new int[countDigit];
+        int index=0;
+        for (int i = 0; i < countNotMatches.length; i++) {
+            if (countNotMatches[i] == 0) {
+                digitsNoUse[index] = arr1[i];
+                index++;
+            }
+        }
+
+        // fill result array no matches digit
+        for (int i = 0; i < countDigit; i++) {
+            result[count] = digitsNoUse[i];
+            count++;
+        }
+
+        return result;
+    }
+
+}
---
+++ src/main/java/easy/relative_sort_array/MainSortArray.java
@@ -0,0 +1,48 @@
+package easy.relative_sort_array;
+
+public class MainSortArray {
+    /*
+   1122. Relative Sort Array
+   Given two arrays arr1 and arr2, the elements of arr2 are distinct,
+   and all elements in arr2 are also in arr1.
+   Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+   Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+   Example 1:
+   Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+   Output: [2,2,2,1,4,3,3,9,6,7,19]
+   Example 2:
+   Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+   Output: [22,28,8,6,17,44]
+
+   Constraints:
+   1 <= arr1.length, arr2.length <= 1000
+   0 <= arr1[i], arr2[i] <= 1000
+   All the elements of arr2 are distinct.
+   Each arr2[i] is in arr1.
+    */
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+        int[] arr2 = new int[]{2, 1, 4, 3, 9, 6};
+        int[] output = new int[] {2,2,2,1,4,3,3,9,6,7,19};
+
+        int[] resultEx1 = new Example1().relativeSortArray(arr1.clone(), arr2);
+        System.out.println(checkResult(resultEx1, output));
+
+        int[] resultSolution1 = new Solution1().relativeSortArray(arr1.clone(), arr2);
+        System.out.println(checkResult(resultSolution1, output));
+
+        int[] resultSolution2 = new Solution2().relativeSortArray(arr1.clone(), arr2);
+        System.out.println(checkResult(resultSolution2, output));
+    }
+
+
+    public static boolean checkResult(int[] arr1, int[] arr2){
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
